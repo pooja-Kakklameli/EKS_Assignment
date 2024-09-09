8c49936b59ec3195d752e3a0655077f7df33a217
pooja-Kakklameli <pooshash11@gmail.com>
2024-09-09 16:09:29 +0530

    Update eks_code_stack.py

--- eks_code/eks_code_stack.py
+++ eks_code/eks_code_stack.py
@@ -25,6 +25,17 @@ class EksCodeStack(Stack):
             default_capacity=2
         )
 
+        eks_role = iam.Role.from_role_arn(self, "EksRole", "arn:aws:iam::009160050626:role/EksCodeStack-MyClusterRoleBA20FE72-6pkuiB5DwRIj")
+
+        # Add additional policies if needed
+        eks_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEKSWorkerNodePolicy"))
+        eks_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2ContainerRegistryReadOnly"))
+
+        existing_user = iam.User.from_user_name(self, "ExistingIamUser", "pooja")  # Replace with your existing IAM user
+
+        # Map the IAM user to Kubernetes RBAC (roles or groups) in the aws-auth ConfigMap
+        cluster.aws_auth.add_user_mapping(existing_user, groups=["system:masters"])
+
         # Create an SSM Parameter (assuming this stores environment like "development")
         ssm_parameter = ssm.StringParameter(
             self, "AccountEnvParam",
@@ -32,12 +43,25 @@ class EksCodeStack(Stack):
             string_value="development",  # Replace with your value
         )
 
+        # Lambda Role with permissions to read from SSM
+        lambda_role = iam.Role(self, "LambdaExecutionRole",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMReadOnlyAccess")
+            ]
+        )
+
         # Create the Lambda Function for the Custom Resource
         custom_function = _lambda.Function(
             self, "CustomResourceFunction",
             runtime=_lambda.Runtime.PYTHON_3_9,
             handler="index.handler",
-            code=_lambda.Code.from_asset("lambda")
+            code=_lambda.Code.from_asset("lambda"),
+            environment={
+                'SSM_PARAM_NAME': ssm_parameter.parameter_name
+            },
+            role=lambda_role
         )
 
         # IAM Policy Statement for the Custom Resource
@@ -60,6 +84,7 @@ class EksCodeStack(Stack):
 
         # Use the Custom Resource output to configure the Helm chart
         ssm_value = custom_resource.get_response_field("Parameter.Value")
+
 
         # Assuming ssm_value is something like "development", translate to replica count
         replicas = 1 if ssm_value == "development" else 3
